1aed8a9add4b012e7a8b55ed5e0a7df018d0d232
박진수 <jingood@bagjinsuui-MacBookAir.local>
2024-05-01 15:39:29 +0900

    product 조회, 등록

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('', views.ProductListAPIView.as_view(), name='product_list'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from .serializers import ProductSerializer
+from .models import Product
 
-# Create your views here.
+
+class ProductListAPIView(APIView):
+
+    def get(self, request):
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
