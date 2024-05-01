7c0dfb47e642518c1efc571883133e92d0e761c2
박진수 <jingood@bagjinsuui-MacBookAir.local>
2024-05-01 17:24:32 +0900

    로그인과 Access Token 재발급 기능

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 from . import views
 
 urlpatterns = [
     path('', views.AccountSignupAPIView.as_view(), name='signup'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
