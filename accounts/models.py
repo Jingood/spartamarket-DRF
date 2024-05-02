331a41529e434325dc05d74b44139aefee7692e8
박진수 <jingood@bagjinsuui-MacBookAir.local>
2024-05-02 14:20:12 +0900

    성별, 자기소개 추가

--- accounts/models.py
+++ accounts/models.py
@@ -2,7 +2,13 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
+    GENDER_CHOICES = (
+        ("M", "Man"),
+        ("W", "Woman"),
+    )
     name = models.CharField(max_length=10)
     nickname = models.CharField(max_length=50)
     birth = models.DateField()
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES, blank=True)
+    intro = models.TextField(blank=True)
     REQUIRED_FIELDS = ['name', 'nickname', 'birth']
--- accounts/serializers.py
+++ accounts/serializers.py
@@ -4,7 +4,7 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['username', 'password', 'email', 'name', 'nickname', 'birth']
+        fields = ['username', 'password', 'email', 'name', 'nickname', 'birth', 'gender', 'intro']
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
@@ -14,12 +14,14 @@ class UserSerializer(serializers.ModelSerializer):
 class UserUpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['username', 'email', 'name', 'nickname', 'birth']
+        fields = ['username', 'email', 'name', 'nickname', 'birth', 'gender', 'intro']
 
     def update(self, instance, validated_data):
         instance.username = validated_data.get('username', instance.username)
         instance.email = validated_data.get('email', instance.email)
         instance.nickname = validated_data.get('nickname', instance.nickname)
         instance.birth = validated_data.get('birth', instance.birth)
+        instance.gender = validated_data.get('gender', instance.gender)
+        instance.intro = validated_data.get('intro', instance.intro)
         instance.save()
         return instance
