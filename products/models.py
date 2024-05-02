d67b5535f705ce1d8221f1dc3c1e01ff7730322f
박진수 <jingood@bagjinsuui-MacBookAir.local>
2024-05-02 15:52:56 +0900

    image필드 수정

--- products/models.py
+++ products/models.py
@@ -5,7 +5,7 @@ class Product(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     price = models.PositiveIntegerField()
-    image = models.ImageField(upload_to='images/', blank=True)
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE
     )
